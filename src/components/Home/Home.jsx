e7e2e2d0956260fcfd4d39395c39b5cc54a76be6
João Vitor <devjohncb@gmail.com>
2023-12-19 23:23:45 -0300

    adding tailwind in the project / creating the overall structure / starting the Home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="home-section">
+      <div className="home-description">
+        <h1 className="text-white">Under Sound Records</h1>
+        <h5 className="text-white w-[70%]">
+          Transforme os seus sonhos na melhor melodia
+        </h5>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      primary: "Rock Salt",
+      secundary: "RocknRoll One",
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {
+        black: "#000000",
+        eerie: "#1B1B1B",
+        jet: "#2d2d2d",
+        pumpkin: "#FF812D",
+      },
+      backgroundImage: {
+        home: "url('./home-tapes.png')",
+        about: "url('./about-studio.png')",
+        services: "url('./services-mic.png')",
+      },
+    },
+  },
+  plugins: [],
+};

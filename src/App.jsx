d8a0280d81cb76ab9fc3683377a9ff7ca1e77d87
João Vitor <devjohncb@gmail.com>
2024-01-02 16:43:41 -0300

    Header component created / solving the responsive header problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./components/Home/Home";
+
 import Header from "./components/global/Header";
 
 const App = () => {
--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,33 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FaInstagram, FaSpotify, FaYoutube } from "react-icons/fa6";
 import { LuCassetteTape } from "react-icons/lu";
+import { MdClose } from "react-icons/md";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // const changeNav = () => {
+  //   if (window.innerWidth < 768) {
+  //     setNavOpen(!navOpen);
+  //   }
+  // };
+
   return (
     <header>
-      <div className="flex items-baseline gap-x-[5px]">
-        <h5 className="font-primary text-pumpkin">US</h5>
-        <h6 className="font-primary text-white">Records</h6>
-      </div>
-      <nav className="hidden flex-col md:flex md:flex-row md:gap-x-8">
-        <a href="#" className="text-white">
-          Início
-        </a>
-        <a href="#" className="text-white">
-          Sobre
-        </a>
-        <a href="#" className="text-white">
-          Serviços
-        </a>
-        <a href="#" className="text-white">
-          Contato
-        </a>
-      </nav>
-      <div className="hidden sm:flex">
-        <FaYoutube className="text-white text-[1.125rem] sm:text-[1.625rem] md:text-[1.825rem] lg:text-[2.125rem]" />
-        <FaSpotify className="text-white text-[1.125rem] sm:text-[1.625rem] md:text-[1.825rem] lg:text-[2.125rem]" />
-        <FaInstagram className="text-white text-[1.125rem] sm:text-[1.625rem] md:text-[1.825rem] lg:text-[2.125rem]" />
+      <div className="header-container">
+        <div className="flex items-baseline gap-x-[5px] mb-0">
+          <h5 className="font-primary text-pumpkin">US</h5>
+          <h6 className="font-primary text-white">Records</h6>
+        </div>
+
+        <div
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="display md:hidden"
+        >
+          {menuOpen ? (
+            <MdClose className="text-pumpkin header-menu-icon" />
+          ) : (
+            <LuCassetteTape className="text-white header-menu-icon" />
+          )}
+        </div>
+
+        <nav
+          className={`${
+            menuOpen
+              ? "flex flex-col md:flex-row justify-center m-[2rem_0rem] md:m-0"
+              : "hidden md:flex"
+          }`}
+        >
+          <a href="#" className="text-white nav-link-hover">
+            <a href="/">Início</a>
+          </a>
+          <a href="#" className="text-white nav-link-hover">
+            Sobre
+          </a>
+          <a href="#" className="text-white nav-link-hover">
+            Serviços
+          </a>
+          <a href="#" className="text-white nav-link-hover">
+            Contato
+          </a>
+        </nav>
+        <div
+          className={`header-social-container ${
+            menuOpen ? "flex flex-row justify-center" : "hidden md:flex"
+          }`}
+        >
+          <FaYoutube className="header-social-icon header-social-icon-hover" />
+          <FaSpotify className="header-social-icon header-social-icon-hover" />
+          <FaInstagram className="header-social-icon header-social-icon-hover" />
+        </div>
       </div>
     </header>
   );
